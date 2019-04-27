50fbbbbe232e8aea997cd3f01766b2a774c1ed8e
Franziska Zander <franzi82@gmx.de>
2019-04-27 21:32:06 +0200

    restructure layout; repair navigation; repair download #6 #11

--- Controllers/BaseAdminFileManagementController.cs
+++ Controllers/BaseAdminFileManagementController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BExIS.Security.Services.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -55,6 +57,45 @@ namespace BExIS.Modules.Fmt.UI.Controllers
             System.IO.File.Move(Path.Combine(AppConfiguration.DataPath, filePath), des);
 
             return null;
+        }
+
+        public ActionResult DownloadFile(string path, string mimeType)
+        {
+           
+            string title = path.Split('\\').Last();
+            string message = string.Format("file was downloaded");
+            string userName = GetUsernameOrDefault();
+            Vaiona.Logging.LoggerFactory.LogCustom(message);
+
+            string message_mail = $"Dataset <b>\"{title}\"</b> with id was downloaded";
+
+
+            if (!string.IsNullOrEmpty(userName))
+            {
+                message_mail += $" by <b>{userName}</b>";
+            }
+
+            message_mail = message_mail + ".";
+
+            var es = new Security.Services.Utilities.EmailService();
+                es.Send(MessageHelper.GetDownloadDatasetHeader(),
+                    message_mail,
+                    ConfigurationManager.AppSettings["SystemEmail"]
+                    );
+
+            return File(Path.Combine(AppConfiguration.DataPath, path), mimeType, title);
+        }
+
+        public string GetUsernameOrDefault()
+        {
+            string username = string.Empty;
+            try
+            {
+                username = HttpContext.User.Identity.Name;
+            }
+            catch { }
+
+            return !string.IsNullOrWhiteSpace(username) ? username : "DEFAULT";
         }
     }
 }
--- Models/InfoPageModel.cs
+++ Models/InfoPageModel.cs
@@ -31,7 +31,9 @@ namespace BExIS.Modules.FMT.UI.Models
         public string Filename { get; set; }
         public string Filepath { get; set; }
         public string MimeType { get; set; }
-        public byte[] FileContent { get;set; } 
+        public byte[] FileContent { get;set; }
+        public DateTime Date { get; set; }
+        public int Length{ get; set; }
 
         public FileModel()
         {
@@ -53,7 +55,9 @@ namespace BExIS.Modules.FMT.UI.Models
             foreach (var file in Directory.GetFiles(Path.Combine(AppConfiguration.DataPath,FMTMenuItemPath)))
             {
                 var fileName = Path.GetFileName(file);
-                fileModels.Add(new FileModel() { Filename = fileName, Filepath =FMTMenuItemPath + @"\\"+ fileName, MimeType = MimeMapping.GetMimeMapping(file) });
+                var filepath = FMTMenuItemPath + @"\\" + fileName;
+
+                fileModels.Add(new FileModel() { Filename = fileName, Filepath = filepath, MimeType = MimeMapping.GetMimeMapping(file),Date = File.GetCreationTime(Path.Combine(AppConfiguration.DataPath, filepath)) , Length = file.Length});
             }
             return fileModels;
         }
