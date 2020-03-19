dbb7829e3f35bf31d0c1ec02efe3e03c4a8c29d2
Franziska Zander <franzi82@gmx.de>
2020-03-19 23:11:51 +0100

    Add help on top for Fieldwork Documents #https://github.com/bexis/BExIS-intern/issues/45

--- Controllers/FieldworkFilesController.cs
+++ Controllers/FieldworkFilesController.cs
@@ -6,7 +6,7 @@ namespace BExIS.Modules.Fmt.UI.Controllers
     {
         public ActionResult Index(string node_id = "")
         {
-            return RedirectToAction("Show", "FieldworkFiles", new { area = "FMT", viewName = "FieldworkFiles", rootMenu = "Fieldwork", node_id = node_id.Replace("\\", "_") });
+            return RedirectToAction("Show", "Fieldwork Documents", new { area = "FMT", viewName = "FieldworkFiles", rootMenu = "Fieldwork", node_id = node_id.Replace("\\", "_") });
         }
     }
 }
