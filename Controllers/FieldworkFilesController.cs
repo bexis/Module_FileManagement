c0adee7e52ca969f78e9dcad41a9fd4dc6a47f80
Petzold <eleonora.petzold@uni-jena.de>
2021-07-27 14:18:37 +0200

    Add green line title close #45

--- Controllers/FieldworkFilesController.cs
+++ Controllers/FieldworkFilesController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using Vaiona.Web.Extensions;
+using Vaiona.Web.Mvc.Models;
 
 namespace BExIS.Modules.Fmt.UI.Controllers
 {
@@ -6,6 +8,8 @@ namespace BExIS.Modules.Fmt.UI.Controllers
     {
         public ActionResult Index(string node_id = "")
         {
+            ViewBag.Title = PresentationModel.GetViewTitleForTenant("BExIS - Fieldwork Documents", this.Session.GetTenant());
+
             return RedirectToAction("Show", "FieldworkFiles", new { area = "FMT", viewName = "FieldworkFiles", viewTitle = "BExIS - Fieldwork Documents", rootMenu = "Fieldwork", node_id = node_id.Replace("\\", "_") });
         }
     }
