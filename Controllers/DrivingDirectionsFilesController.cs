801ac4d684607b2f3024c188cdfabaf3b0368a22
Petzold <eleonora.petzold@uni-jena.de>
2021-12-17 15:37:31 +0100

    FMT site for Driving directions done and close #52

---
+++ Controllers/DrivingDirectionsFilesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Modules.Fmt.UI.Controllers
+{
+    public class DrivingDirectionsFilesController : BaseFileManagementController
+    {
+        // GET: DrivingDirectionsFiles
+        public ActionResult Index(string node_id = "")
+        {
+            return RedirectToAction("Show", "DrivingDirectionsFiles", new { area = "FMT", viewName = "DrivingDirectionsFiles", viewTitle = "Driving Directions Documents", rootMenu = "DrivingDirections", node_id = node_id.Replace("\\", "_") });
+        }
+    }
+}
