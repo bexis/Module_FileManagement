2a24da1b323bd30a18d15d9c15e346d62002a257
Eleonora Petzold <eleonora.petzold@uni-jena.de>
2024-06-19 18:32:16 +0200

    https://github.com/bexis/BExIS-intern/issues/629

---
+++ Controllers/ThematicGroupsFilesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Modules.Fmt.UI.Controllers
+{
+    public class ThematicGroupsFilesController : BaseFileManagementController
+    {
+        public ActionResult Index(string node_id = "")
+        {
+            return RedirectToAction("Show", "ThematicGroupsFiles", new { area = "FMT", viewName = "ThematicGroupsFiles", viewTitle = "ThematicGroups Documents", rootMenu = "ThematicGroups", node_id = node_id.Replace("\\", "_") });
+        }
+    }
+}
